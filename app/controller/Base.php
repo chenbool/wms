f81e449ee7429b11a02828bef528e033aaa2be35
wuangv <wuangv@gmail.com>
2019-02-13 13:27:57 +0800

    修改菜单显示方式

--- app/controller/Base.php
+++ app/controller/Base.php
@@ -7,16 +7,22 @@ use think\Controller,
 
 class Base extends Controller
 {
-	public function __construct(){
-		parent::__construct();
-    	if( !Session::get('uid','think') ){
+    protected $menuList;
+
+    protected function _initialize()
+    {
+        if( !Session::get('uid','think') ){
             return $this->redirect("Login/index"); 
         }
         $user_info = User::get(['id' => Session::get('uid','think') ]);
-
+        //获取角色
+        $role = $user_info->findRole;
+        
         $service = new MenuService();
-        $this->assign( $service->getMenuShow($user_info) );
+        $this->menuList = $service->getMenuShow($user_info);
 
+        $this->assign( $this->menuList );
+        
     }
     
 }
--- app/service/MenuService.php
+++ app/service/MenuService.php
@@ -1,65 +1,119 @@
 <?php
 namespace app\service;
+
 use app\model\Menu,
     app\model\User;
+use think\Db;
 
 class MenuService
 {
+    protected $menuModel;
 
-    public function getFather(){
-        return Menu::all([
-        	'status'	=>	0,
-        	'level'		=>	0
-        ]);
-    }
-  
-    public function getChild(){
-        return Menu::all([
-        	'status'	=>	0,
-        	'level'		=>	['>',0]
-        ]);
+    public function __construct()
+    {
+        $this->menuModel = new Menu();
     }
 
-    public function getMenu($ids){
-        return [
-            'father'    =>  Menu::all([
-                'status'    =>  0,
-                'level'     =>  0,
-                'id'        =>  ['in',"$ids"]
-            ]),
-            'child'     =>  Menu::all([
-                'status'    =>  0,
-                'level'     =>  ['>',0],
-                'id'        =>  ['in',"$ids"]
-            ])
-        ]; 
-    }  
+    public function getFather()
+    {
+        return $this->menuModel->where([
+            'status' => 0,
+            'level' => 0
+        ])->select();
+    }
 
-    public function getMenuAll(){
-        return [
-            'father'    =>  Menu::all([
-                'status'    =>  0,
-                'level'     =>  0
-            ]),
-            'child'     =>  Menu::all([
-                'status'    =>  0,
-                'level'     =>  ['>',0]
-            ])
-        ]; 
-    } 
+    public function getChild()
+    {
+        return $this->menuModel->where([
+            'status' => 0,
+            'level' => ['>', 0]
+        ])->select();
+    }
 
-    public function getMenuShow($obj){
-        if( $obj->id == '1' ){
-            $_menuList = $this->getMenuAll();
-        }else{
-            $_menuList = $this->getMenu($obj->findRole->ids);
+    public function getMenu($ids)
+    {
+
+        return [
+            'father' => $this->menuModel->where([
+                'status' => 0,
+                'level' => 0,
+                'id' => ['in', "$ids"]
+            ])->select(),
+            'child' => $this->menuModel->where([
+                'status' => 0,
+                'level' => ['>', 0],
+                'id' => ['in', "$ids"]
+            ])->select()
+        ];
+    }
+
+    public function getMenuAll()
+    {
+        return [
+            'father' => $this->menuModel->where([
+                'status' => 0,
+                'level' => 0
+            ])->select(),
+            'child' => $this->menuModel->where([
+                'status' => 0,
+                'level' => ['>', 0]
+            ])->select()
+        ];
+    }
+
+    public function getMenuTree($ids = null)
+    {
+        if (empty($ids)) {
+            $menuList = Db::name('menu')->where([
+                'status' => 0,
+            ])->select();
+        } else {
+            $menuList = Db::name('menu')->where([
+                'status' => 0, 'id' => ['in', "$ids"]
+            ])->select();
+        }
+
+        if(empty($menuList)) return [];
+        return $this->menuTree($menuList);
+
+    }
+
+    public function menuTree(array $menuList,$pid = 0,$child = 'child')
+    {
+        $result = [];
+        foreach($menuList as $key => $val){
+            if($val['pid'] == $pid){
+                $result[] = $val;
+                unset($menuList[$key]);
+            }
+           
+        }
+
+        foreach($result as $key => $val){
+            $result[$key][$child] = $this->menuTree($menuList,$val['id']);
+        }
+        return $result;
+    }
+
+    public function getMenuShow($obj)
+    {
+        //是否是超级管理员
+        if ($obj->id == '1') {
+            $_menuList = $this->getMenuTree();
+        } else {
+            $_menuList = $this->getMenuTree($obj->findRole->ids);
         }
 
         return [
-            'my_info'   =>  $obj ,
-            '_menuList' =>  $_menuList
+            'my_info' => $obj,
+            '_menuList' => $_menuList
         ];
-    } 
+    }
+
+    public function is_auth()
+    {
+
+    }
 
 
 }
